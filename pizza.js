aa621129f29ca24fa211051d4cdbec511594898b
JackWilliamson116 <28870092+JackWilliamson116@users.noreply.github.com>
2022-03-20 18:07:31 -0400

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -1,4 +1,12 @@
-var img = document.createElement('img');
-img.src = 'https://JackWilliamson116.github.io/pizza.jpg';
-document.getElementById('body').appendChild(img);
-down.innerHTML = "Image Element Added."; 
+function display_image(src, width, height, alt) {
+    var a = document.createElement("img");
+    a.src = src;
+    a.width = width;
+    a.height = height;
+    a.alt = alt;
+    document.body.appendChild(a);
+}
+display_image('https://JackWilliamson116.github.io/pizza.jpg', 
+                 276, 
+                 110, 
+                 'JavaScriptImage');

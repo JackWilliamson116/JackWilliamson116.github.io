7b6e356e7a1b2b39ca20853749622db0e29cd9b8
JackWilliamson116 <28870092+JackWilliamson116@users.noreply.github.com>
2022-03-20 17:53:29 -0400

    Update arbitraryscript.js

--- arbitraryscript.js
+++ arbitraryscript.js
@@ -2,7 +2,7 @@ var imgObj = null;
             
 function init() {
   imgObj = document.createElement("img");
-  imgObj.src = "https://JackWilliamson116.github.io/pizza.jpg"
+  imgObj.src = "https://JackWilliamson116.github.io/pizza.jpg";
   imgObj.style.position= 'relative';
   imgObj.style.left = '0px'; 
 }

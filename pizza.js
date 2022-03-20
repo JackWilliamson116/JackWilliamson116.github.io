51e8791876784d0393cfed60c903a72c8c54fac1
JackWilliamson116 <28870092+JackWilliamson116@users.noreply.github.com>
2022-03-20 18:00:19 -0400

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -1,1 +1,5 @@
-
+var imgObj = null;
+imgObj = document.createElement("img");
+imgObj.src = "https://JackWilliamson116.github.io/pizza.jpg";
+imgObj.style.position= 'relative';
+imgObj.style.left = '0px'; 
